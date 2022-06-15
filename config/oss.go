b6241474926df2d28357d6f2621f432028aec487
wujunke <junkewu211@163.com>
2022-06-15 15:11:36 +0800

    fix:

--- config/oss.go
+++ config/oss.go
@@ -8,5 +8,5 @@ const (
 	// OSSAccesskeyID : oss访问key
 	OSSAccesskeyID = "LTAI5t7ZYc54WkJH5a2sC8Rj"
 	// OSSAccessKeySecret : oss访问key secret
-	OSSAccessKeySecret = "LV9d1cZb93LeKkOR4yWRux5MVJo3u2"
+	OSSAccessKeySecret = ""
 )
